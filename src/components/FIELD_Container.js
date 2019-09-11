64edf387616926e27b45df40e29fa96be7c756f1
Nick Zoner <nick.zoner@scubeenterprise.com>
2019-09-11 14:05:19 -0400

    [NZ] Only displays the first required filter of each type per group

--- src/components/FIELD_Container.js
+++ src/components/FIELD_Container.js
@@ -19,21 +19,53 @@ class FIELD_Container extends Component {
             rows.push(
                 <FieldItem key={i} id={i}/>
             );
-            //Add Required Filters (Only the first one)
-            let min = -1;
+
+            // //Code to add all requried Filters
+            // for (let f in this.props.filters) {
+            //     if (typeof this.props.filters[f].key !== "string") continue;
+            //     let filter_id = this.props.filters[f].key.split("-");
+            //     if (filter_id[1] === i) {
+            //         rows.push(<Filter key={this.props.filters[f].key} id={this.props.filters[f].key} req={true}/>)
+            //     }
+            // }
+
+            //Add Required Filters (Only from min row in each group);
+            let min_from_groups = {};
             for (let f in this.props.filters) {
                 if (typeof this.props.filters[f].key !== "string") continue;
-                let filter_id = this.props.filters[f].key.split("-");
-                if (min === -1) min = filter_id[1];
-                else min = Math.min(min, filter_id[1]);
+                let filter_id = parseInt(this.props.filters[f].key.split("-")[1]);
+                if (min_from_groups[this.props.filters[f].group]) {
+                    min_from_groups[this.props.filters[f].group] = Math.min(min_from_groups[this.props.filters[f].group], filter_id);
+                } else {
+                    min_from_groups[this.props.filters[f].group] = filter_id;
+                }
             }
+
+            console.log(min_from_groups);
+
             for (let f in this.props.filters) {
                 if (typeof this.props.filters[f].key !== "string") continue;
-                let filter_id = this.props.filters[f].key.split("-");
-                if (filter_id[1].toString() === min.toString() && filter_id[1] === i) {
+                let filter_id = this.props.filters[f].key.split("-")[1];
+                if (filter_id.toString() === i && filter_id === min_from_groups[this.props.filters[f].group].toString()) {
                     rows.push(<Filter key={this.props.filters[f].key} id={this.props.filters[f].key} req={true}/>)
                 }
             }
+            // let min = -1;
+            // for (let f in this.props.filters) {
+            //     if (typeof this.props.filters[f].key !== "string") continue;
+            //     let filter_id = this.props.filters[f].key.split("-");
+            //     if (this.props.fields[i].group === ) {
+            //         if (min === -1) min = filter_id[1];
+            //         else min = Math.min(min, filter_id[1]);
+            //     }
+            // }
+            // for (let f in this.props.filters) {
+            //     if (typeof this.props.filters[f].key !== "string") continue;
+            //     let filter_id = this.props.filters[f].key.split("-");
+            //     if (filter_id[1].toString() === min.toString() && filter_id[1] === i) {
+            //         rows.push(<Filter key={this.props.filters[f].key} id={this.props.filters[f].key} req={true}/>)
+            //     }
+            // }
         }
         return (
             rows
@@ -67,6 +99,7 @@ class FIELD_Container extends Component {
 const mapStateToProps = state => ({
     fields: state.fields,
     filters: state.filters,
+    groups: state.groups
 });
 
 const mapDispatchToProps = dispatch => ({
